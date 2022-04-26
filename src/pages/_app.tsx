1f23726fbb16b53a6301d968d866581c5bd46c17
j3lte <jwlagendijk@gmail.com>
2022-04-26 16:42:06 +0200

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
+import { MantineProvider } from '@mantine/core';
+import { GoogleAnalytics, usePagesViews } from 'nextjs-google-analytics';
 
 import '@/styles/globals.css'
-import { MantineProvider } from '@mantine/core';
 
 import { StoreProvider } from '@/context/StoreContext';
 import { UIProvider } from '@/context/UIContext';
@@ -13,12 +14,13 @@ import { defaultSeo } from '@/util/seo';
 const mainArtStore = new ArtStore();
 
 const App = ({ Component, pageProps }: AppProps ) => {
+    usePagesViews();
     const [artStore] = useState(mainArtStore);
 
     return (
         <>
             <DefaultSeo {...defaultSeo} />
-
+            <GoogleAnalytics />
             <StoreProvider store={artStore}>
                 <MantineProvider
                     withGlobalStyles
