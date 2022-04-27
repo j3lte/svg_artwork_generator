230d9cdf5a13edfa7aed4dac165ab56e429444a9
j3lte <jwlagendijk@gmail.com>
2022-04-27 14:42:52 +0200

    Randomize empty bg/fg

--- src/util/generators/empty.ts
+++ src/util/generators/empty.ts
@@ -2,9 +2,11 @@ import { Rect } from "@svgdotjs/svg.js";
 import { drawRect, CreatorFunc, getGroup } from "./common";
 
 export const generator: CreatorFunc = (drawer, block, opts): Rect => {
+    const color = block.opts.coin ? block.color.fg : block.color.bg;
     return drawRect(
         getGroup(drawer, 'em', opts?.optimize),
-        block
+        block,
+        color
     );
 }
 
